e12dfbbc2092c492cce1fb5a01110bc96ccfee7d
DiiegoF. <168261206+dev-website-design@users.noreply.github.com>
2026-07-27 18:23:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(reg => console.log("Service Worker registrado:", reg))
+      .catch(err => console.error("Error al registrar el Service Worker:", err));
+  });
+}
